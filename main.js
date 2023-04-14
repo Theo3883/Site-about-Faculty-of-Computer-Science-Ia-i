b6eabeccff37ea1deb908caf40b70b69d1aa51e7
theo3883 <teosandu88@mail.com>
2023-04-14 18:15:47 +0300

    new commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var text = document.getElementById("text");
+
+text.addEventListener("animationend", function() {
+  text.classList.add("no-border");
+});
+var text = document.getElementById("text");
+
+text.addEventListener("animationend", function() {
+  text.classList.add("no-border");
+});
+
